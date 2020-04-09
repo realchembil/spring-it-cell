481bb863a041c2780140c765c7bff831abd1a9b0
Surjith Sudhakaran <surjithsudhakaran@Surjiths-Air.home>
2020-04-09 19:45:15 -0400

    Initial Commit

---
+++ spring-it-cell/src/main/java/com/devapps/SpringItCellApplication.java
@@ -0,0 +1,13 @@
+package com.devapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringItCellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringItCellApplication.class, args);
+	}
+
+}
